363e72623a0cc0a0b00472c75121628f66e2698e
Wangjingbo <jingbo.wang@fenrir-inc.com.cn>
2025-05-23 17:39:15 +0800

    try

--- arceos/exercises/simple_hv/src/main.rs
+++ arceos/exercises/simple_hv/src/main.rs
@@ -3,29 +3,31 @@
 #![feature(asm_const)]
 #![feature(riscv_ext_intrinsics)]
 
+extern crate alloc;
 #[cfg(feature = "axstd")]
 extern crate axstd as std;
-extern crate alloc;
 #[macro_use]
 extern crate axlog;
 
+mod csrs;
+mod loader;
+mod regs;
+mod sbi;
 mod task;
 mod vcpu;
-mod regs;
-mod csrs;
-mod sbi;
-mod loader;
 
-use vcpu::VmCpuRegisters;
-use riscv::register::{scause, sstatus, stval};
-use csrs::defs::hstatus;
-use tock_registers::LocalRegisterCopy;
-use csrs::{RiscvCsrTrait, CSR};
-use vcpu::_run_guest;
-use sbi::SbiMessage;
-use loader::load_vm_image;
-use axhal::mem::PhysAddr;
+use crate::csrs::traps;
 use crate::regs::GprIndex::{A0, A1};
+use axhal::mem::PhysAddr;
+use csrs::defs::hstatus;
+use csrs::{RiscvCsrTrait, CSR};
+use loader::load_vm_image;
+use riscv::register::scause::Interrupt;
+use riscv::register::{scause, sstatus, stval};
+use sbi::SbiMessage;
+use tock_registers::LocalRegisterCopy;
+use vcpu::VmCpuRegisters;
+use vcpu::_run_guest;
 
 const VM_ENTRY: usize = 0x8020_0000;
 
@@ -50,8 +52,7 @@ fn main() {
     prepare_vm_pgtable(ept_root);
 
     // Kick off vm and wait for it to exit.
-    while !run_guest(&mut ctx) {
-    }
+    while !run_guest(&mut ctx) {}
 
     panic!("Hypervisor ok!");
 }
@@ -68,9 +69,11 @@ fn prepare_vm_pgtable(ept_root: PhysAddr) {
 }
 
 fn run_guest(ctx: &mut VmCpuRegisters) -> bool {
+    ax_println!("Entering guest...");
     unsafe {
         _run_guest(ctx);
     }
+    ax_println!("Returned from guest.");
 
     vmexit_handler(ctx)
 }
@@ -93,26 +96,61 @@ fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
                         assert_eq!(a0, 0x6688);
                         assert_eq!(a1, 0x1234);
                         ax_println!("Shutdown vm normally!");
+
+                        // ctx.guest_regs.sepc += 4;
                         return true;
-                    },
-                    _ => todo!(),
+                    }
+                    _ => {
+                        // ctx.guest_regs.sepc += 4;
+                    }
                 }
             } else {
                 panic!("bad sbi message! ");
             }
-        },
+        }
         Trap::Exception(Exception::IllegalInstruction) => {
-            panic!("Bad instruction: {:#x} sepc: {:#x}",
+            let instr = stval::read() as u32;
+            // 根据指令类型进行相应处理
+            if instr == 0xf14025f3 {
+                ctx.guest_regs.gprs.set_reg(A0, 0x6688);
+                ctx.guest_regs.gprs.set_reg(A1, 0x1234);
+                // 处理完成后调整 sepc
+                ctx.guest_regs.sepc += 4;
+                ax_println!(
+                    "Bad instruction: {:#x} sepc: {:#x}",
+                    stval::read(),
+                    ctx.guest_regs.sepc
+                );
+
+                return false;
+            }
+
+            panic!(
+                "Bad instruction: {:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
-        },
+        }
         Trap::Exception(Exception::LoadGuestPageFault) => {
-            panic!("LoadGuestPageFault: stval{:#x} sepc: {:#x}",
+            ctx.guest_regs.sepc += 4;
+            
+            return false;
+            panic!(
+                "LoadGuestPageFault: stval{:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
-        },
+        }
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            info!("timer irq emulation");
+            // Enable guest timer interrupt
+            CSR.hvip
+                .read_and_set_bits(traps::interrupt::VIRTUAL_SUPERVISOR_TIMER);
+            // Clear host timer interrupt
+            CSR.sie
+                .read_and_clear_bits(traps::interrupt::SUPERVISOR_TIMER);
+            return false;
+        }
         _ => {
             panic!(
                 "Unhandled trap: {:?}, sepc: {:#x}, stval: {:#x}",
@@ -127,9 +165,8 @@ fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
 
 fn prepare_guest_context(ctx: &mut VmCpuRegisters) {
     // Set hstatus
-    let mut hstatus = LocalRegisterCopy::<usize, hstatus::Register>::new(
-        riscv::register::hstatus::read().bits(),
-    );
+    let mut hstatus =
+        LocalRegisterCopy::<usize, hstatus::Register>::new(riscv::register::hstatus::read().bits());
     // Set Guest bit in order to return to guest mode.
     hstatus.modify(hstatus::spv::Guest);
     // Set SPVP bit in order to accessing VS-mode memory from HS-mode.
